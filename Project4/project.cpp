6b54fdb39d3fb0b565dfae92aef123d82c9acc9c
solocarrot <whddbs3595@naver.com>
2026-09-04 10:40:19 +0900

    "데이터형식을 조정하는걸 배움"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -6,3 +6,18 @@
 //	return 0;
 //}
 
+//진수바꾸기
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num = 1237;
+	cout << num << endl;
+	cout << oct << num << endl;
+	cout << hex<< num << endl;
+
+	cout << showbase << oct << num << endl;
+	cout << showbase << hex << num << endl;
+
+}
