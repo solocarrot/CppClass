567cc02545fadc40416c54b17afb330ea7daa329
solocarrot <rlawhddbs3339@gmail.com>
2026-09-09 17:30:40 +0900

    "별찍기 문제에서 space와 별의 개수를 가지고 놀면서 플레이해봄 근데 왕복을 어떻게 해결해야될지 모르겠음"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -1,23 +1,48 @@
+////#include <iostream>
+////using namespace std;
+////
+////int main() {
+////	cout << "hello world" << endl;
+////	return 0;
+////}
+//
+////진수바꾸기
+//
 //#include <iostream>
 //using namespace std;
 //
 //int main() {
-//	cout << "hello world" << endl;
-//	return 0;
+//	int num = 1237;
+//	cout << num << endl;
+//	cout << oct << num << endl;
+//	cout << hex << num << endl;
+//
+//	cout << showbase << oct << num << endl;
+//	cout << showbase << hex << num << endl;
+//
 //}
 
-//진수바꾸기
+//3. 별찍기.
 
 #include <iostream>
-using namespace std;
 
 int main() {
-	int num = 1237;
-	cout << num << endl;
-	cout << oct << num << endl;
-	cout << hex<< num << endl;
+	int num = 3;
+	int space = num - 1;
+	int variableStar = num;
 
-	cout << showbase << oct << num << endl;
-	cout << showbase << hex << num << endl;
+	for (int i = space; i <= 0; i--) {
+		std::cout << " " * i << "*" * variableStar << std::endl;
+		variableStar = variableStar + 2;
+		if (i == 0) {
+			for (int j = 0; j < num; j++) {
+				std::cout << "*" * variableStar << std::endl;
+			}
+		}
+		for (int j = 1; j < num; j++) {
+			space = j;
+		}
+	}
+
 
 }
