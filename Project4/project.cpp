0fa970f3467796eb789826c13048a230420eee5b
solocarrot <rlawhddbs3339@gmail.com>
2026-09-11 00:53:01 +0900

    "구구단 출력 문제해결해봄 for문안에 내부 for문을 만들면 어떻게 출력되는지랑 사이사이에 공백을 어떻게 만들어야되는지에 대해서 생각해봤는데 생각대로 출력이 되지않음"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -47,27 +47,93 @@
 //
 //}
 
-//3. 별찍기.
+
+//구구단
+
+////3. 별찍기.
+//
+//#include <iostream>
+//
+//int main() {
+//	int num = 3;
+//	int space = num - 1;
+//	int variableStar = num;
+//
+//	for (int i = space; i <= 0; i--) {
+//		std::cout << " " * i << "*" * variableStar << std::endl;
+//		variableStar = variableStar + 2;
+//		if (i == 0) {
+//			for (int j = 0; j < num; j++) {
+//				std::cout << "*" * variableStar << std::endl;
+//			}
+//		}
+//		for (int j = 1; j < num; j++) {
+//			space = j;
+//		}
+//	}
+//}
+
+// 4.구구단
 
 #include <iostream>
 
 int main() {
-	int num = 3;
-	int space = num - 1;
-	int variableStar = num;
 
-	for (int i = space; i <= 0; i--) {
-		std::cout << " " * i << "*" * variableStar << std::endl;
-		variableStar = variableStar + 2;
-		if (i == 0) {
-			for (int j = 0; j < num; j++) {
-				std::cout << "*" * variableStar << std::endl;
+	int n = 11;
+	int lineForNum = 4;
+	int nowNum = 1;
+
+	for (int p = 0; p < n / lineForNum; p++) {
+		for (int i = 1; i <= 7; i++) {
+			std::cout << nowNum << "*" << i << std::endl;
+			nowNum++;
+			for (int j = 0; j < lineForNum - 1; j++) {
+				std::cout << " " << std::endl;
+				std::cout << nowNum << "*" << i << "=" << nowNum * i;
+				nowNum++;
+				}
 			}
+			std::cout << std::endl;
 		}
-		for (int j = 1; j < num; j++) {
-			space = j;
-		}
+		std::cout << std::endl;
+		return 0;
 	}
 
+// 5. 완전수와 소수출력
 
-}
+//#include <iostream>
+//
+//int main() {
+//	using namespace std;
+//	int x;
+//	int numberElement = 1;
+//	bool isDecimal = true;
+//	bool isComplete = false;
+//
+//	std::cin >> x;
+//
+//	for (int i = 2; i <= x; i++) {
+//		for (int j = 2; j <= i / 2; j++) {
+//			while (isDecimal == true) {
+//				if (i % j == 0 && i / j > 1) {
+//					isDecimal = false;
+//				}
+//			}
+//			if (isDecimal == false) {
+//				//소수가 아닌 수들에 대해서 완전수인지에 대한 검사를 진행
+//				for (int k = 2; k <= j / 2; k++) {
+//					if (j % k == 0 && k * k != j) {
+//						numberElement += k;
+//						numberElement += j / k;
+//					}
+//				}
+//		}
+//			}
+//	}
+//
+//	for (int i = 2; i <= x; i++) {
+//
+//	}
+//	
+//	return 0;
+//}
