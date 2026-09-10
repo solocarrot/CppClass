f8740aafdc8bef93ec1c5d543375b4b5fd512608
solocarrot <rlawhddbs3339@gmail.com>
2026-09-11 00:22:33 +0900

    "오류발생"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -21,28 +21,30 @@
 //	cout << showbase << hex << num << endl;
 //
 //}
+//
+////3. 별찍기.
+//
+//#include <iostream>
+//
+//int main() {
+//	int num = 3;
+//	int space = num - 1;
+//	int variableStar = num;
+//
+//	for (int i = space; i <= 0; i--) {
+//		std::cout << " " * i << "*" * variableStar << std::endl;
+//		variableStar = variableStar + 2;
+//		if (i == 0) {
+//			for (int j = 0; j < num; j++) {
+//				std::cout << "*" * variableStar << std::endl;
+//			}
+//		}
+//		for (int j = 1; j < num; j++) {
+//			space = j;
+//		}
+//	}
+//
+//
+//}
 
-//3. 별찍기.
-
-#include <iostream>
-
-int main() {
-	int num = 3;
-	int space = num - 1;
-	int variableStar = num;
-
-	for (int i = space; i <= 0; i--) {
-		std::cout << " " * i << "*" * variableStar << std::endl;
-		variableStar = variableStar + 2;
-		if (i == 0) {
-			for (int j = 0; j < num; j++) {
-				std::cout << "*" * variableStar << std::endl;
-			}
-		}
-		for (int j = 1; j < num; j++) {
-			space = j;
-		}
-	}
-
-
-}
+//구구단
