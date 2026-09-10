6237dcfbb93549268bb030dd975992ab8e4e98df
solocarrot <whddbs3595@naver.com>
2026-09-10 14:28:28 +0900

    "구구단 출력문제를 풀다가 지능의 부족을 느껴버림"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -22,27 +22,48 @@
 //
 //}
 
-//3. 별찍기.
+////3. 별찍기.
+//
+//#include <iostream>
+//
+//int main() {
+//	int num = 3;
+//	int space = num - 1;
+//	int variableStar = num;
+//
+//	for (int i = space; i <= 0; i--) {
+//		std::cout << " " * i << "*" * variableStar << std::endl;
+//		variableStar = variableStar + 2;
+//		if (i == 0) {
+//			for (int j = 0; j < num; j++) {
+//				std::cout << "*" * variableStar << std::endl;
+//			}
+//		}
+//		for (int j = 1; j < num; j++) {
+//			space = j;
+//		}
+//	}
+//}
+
+// 4.구구단
 
 #include <iostream>
 
 int main() {
-	int num = 3;
-	int space = num - 1;
-	int variableStar = num;
 
-	for (int i = space; i <= 0; i--) {
-		std::cout << " " * i << "*" * variableStar << std::endl;
-		variableStar = variableStar + 2;
-		if (i == 0) {
-			for (int j = 0; j < num; j++) {
-				std::cout << "*" * variableStar << std::endl;
+	int n = 11;
+	int lineForNum = 4;
+
+	for (int p = 0; p < n; p += lineForNum) {
+		for (int i = 1; i <= 7; i++) {
+			for (int j = p + 1; j <= p + 1 + lineForNum; j++) {
+				std::cout << j << "*" << i << "=" << j * i;
+				for (int k = 0; k < 3; k++) {
+					std::cout << " ";
+				}
 			}
-		}
-		for (int j = 1; j < num; j++) {
-			space = j;
-		}
+			std::cout << std::endl;
 	}
-
-
+	}
+	return 0;
 }
