a48c24e5e59cc24657840f02325a2952b468fda2
solocarrot <rlawhddbs3339@gmail.com>
2026-09-12 17:05:25 +0900

    "재귀함수 완벽구현함 (아마도)"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -222,75 +222,86 @@
 //	}
 //}
 
-//// 6.재귀함수
+// 6.재귀함수
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    void printReverseDigit(int n);
+    void printDigit(int n);
+
+    int cnt, val;
+    cin >> cnt;
+
+    for (int i = 0; i < cnt; ++i) {
+        cin >> val;
+
+        cout << "Backward : ";
+        printReverseDigit(val);
+
+        cout << endl << "Forward : ";
+        printDigit(val);
+        cout << endl;
+    }
+    return 0;
+}
+
+void printReverseDigit(int n) {
+    if (n >= 10) {
+        cout << n % 10 << " ";
+        printReverseDigit(n / 10);
+    }
+    else {
+        cout << n ;
+    }
+}
+
+void printDigit(int n) {
+    if (n >= 10) {
+        printDigit(n / 10);
+    }
+    std::cout << n % 10 << " ";
+    
+}
+
+//// 7. 랜드함수
+//
+//#include <iostream>
+//#include <cstdlib>
+//#include <ctime>
+//
+//int main() {
+//	int answer = (rand() % 100) + 1;
+//	int cnt = 1;
+//	int submitNum = (rand() % 100) + 1;
+//	int ceilingNum = 100;
+//	int floorNum = 0;
+//
+//	while (answer != submitNum) {
+//		if (submitNum < answer) {
+//			floorNum = submitNum;
+//			submitNum = (rand() % (ceilingNum - floorNum)) + (floorNum + 1) ;
+//			std::cout << submitNum << std::endl;
+//			cnt++;
+//		}
+//		else if (submitNum > answer) {
+//			ceilingNum = submitNum;
+//			submitNum = (rand() % (ceilingNum - floorNum)) + (floorNum + 1);
+//			std::cout << submitNum << std::endl;
+//			cnt++;
+//		}
+//	}
+//	std::cout << answer << std::endl;
+//	std::cout << "how try " << cnt << std::endl;
+//	
+//}
+
+//// 8.팩토리얼구하기
 //
 //#include <iostream>
 //
-//using namespace std;
-//
 //int main() {
-//    void printReverseDigit(int n);
-//    void printDigit(int n);
-//
-//    int cnt, val;
-//    cin >> cnt;
-//
-//    for (int i = 0; i < cnt; ++i) {
-//        cin >> val;
-//
-//        cout << "Backward :";
-//        printReverseDigit(val);
-//
-//        cout << endl << "Forward :";
-//        printDigit(val);
-//        cout << endl;
-//    }
-//    return 0;
+//	
 //}
-//
-//void printReverseDigit(int n) {
-//    if (n >= 10) {
-//        cout << n % 10;
-//        printReverseDigit(n / 10);
-//    }
-//    else {
-//        cout << n;
-//    }
-//
-//}
-//
-//void printDigit(int n) {
-//    
-//}
-
-// 7. 랜드함수
-
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-int main() {
-	int answer = (rand() % 100) + 1;
-	int cnt = 1;
-	int submitNum = (rand() % 100) + 1;
-	int ceilingNum = 100;
-	int floorNum = 0;
-
-	while (answer != submitNum) {
-		if (submitNum < answer) {
-			floorNum = submitNum;
-			submitNum = (rand() % (ceilingNum - floorNum)) + (floorNum + 1) ;
-			std::cout << submitNum << std::endl;
-			cnt++;
-		}
-		else if (submitNum > answer) {
-			ceilingNum = submitNum;
-			submitNum = (rand() % (ceilingNum - floorNum)) + (floorNum + 1);
-			std::cout << submitNum << std::endl;
-			cnt++;
-		}
-	}
-	std::cout << answer << std::endl;
-	std::cout << "how try " << cnt << std::endl;
-	
-}
