54850f1cd14df9e2568d0436b76b64b81cdb4fd4
solocarrot <rlawhddbs3339@gmail.com>
2026-09-14 01:03:54 +0900

    "2번 문제도 쉽게 해결함"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -33,6 +33,31 @@
 //}
 // 
  
+
+// 2.정수의 가장큰자리와 수 출력하기. 完
+
+#include <iostream>
+
+int main() {
+
+	int cnt;
+	std::cin >> cnt;
+
+	for (int i = 0; i < cnt; i++) {
+		int num;
+		std::cin >> num;
+
+		int intLength = 1;
+
+		while ((num / 10) != 0) {
+			num = num / 10;
+			intLength++;
+		}
+
+		std::cout << num % 10 << " @ [" << intLength << "]" << std::endl;
+	}
+	return 0;
+}
  
  
  
