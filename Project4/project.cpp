bad5f54a6a9c8ca38363a254fab53f21db40987b
solocarrot <rlawhddbs3339@gmail.com>
2026-09-14 00:30:51 +0900

    "구구단 이걸 해내노"

--- Project4/project.cpp
+++ Project4/project.cpp
@@ -95,35 +95,41 @@
 //	}
 //}
 
-// 4.구구단
-
-#include <iostream>
-
-int main() {
-
-	int n = 11;
-	int lineForNum = 4;
-	int nowNum = 1;
-
-	for (int p = 0; p <= (n / lineForNum); p++) {
-		for (int i = 1; i <= 7; i++) {
-			std::cout << nowNum << " * " << i << " = " << nowNum * i;
-			nowNum++;
-			for (int j = 0; j < lineForNum - 1 ; j++) {
-				if (nowNum <= n) {
-					std::cout << "	";
-					std::cout << nowNum << " * " << i << " = " << nowNum * i;
-					nowNum++;
-				}
-			}
-			std::cout << std::endl;
-			nowNum -= lineForNum;
-		}
-		nowNum += lineForNum;
-		std::cout << std::endl;
-	}
-	return 0;
-}
+//// 4.구구단 完
+//
+//#include <iostream>
+//
+//int main() {
+//	int cnt;
+//	std::cin >> cnt;
+//
+//	for (int k = 0; k < cnt; k++) {
+//
+//		int n , secondNumber,lineForNum;
+//		std::cin >> n >> secondNumber >> lineForNum;
+//		int nowNum = 1;
+//
+//		for (int p = 0; p <= (n / lineForNum); p++) {
+//			for (int i = 1; i <= secondNumber; i++) {
+//				std::cout << nowNum << " * " << i << " = " << nowNum * i;
+//				nowNum++;
+//				for (int j = 0; j < lineForNum - 1; j++) {
+//					if (nowNum <= n) {
+//						std::cout << "	";
+//						std::cout << nowNum << " * " << i << " = " << nowNum * i;
+//						nowNum++;
+//					}
+//				}
+//				std::cout << std::endl;
+//				if (p == (n / lineForNum)) { nowNum -= (nowNum - (lineForNum * p + 1)); }
+//				else { nowNum -= lineForNum; }
+//			}
+//			nowNum += lineForNum;
+//			std::cout << std::endl;
+//		}
+//	}
+//	return 0;
+//}
 //
 //// 5. 완전수와 소수출력
 //
