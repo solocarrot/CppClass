e3b2e67e1e49f85cea218cc7d96c7adff10c1a0e
solocarrot <whddbs3595@naver.com>
2026-09-18 13:27:44 +0900

    "날짜출력 따라적기했음"

--- Project4/enter.cpp
+++ Project4/enter.cpp
@@ -47,3 +47,78 @@
 //	cout << "평균점수는 " << getAverage(stdScoreList, stdNum);
 //}
 
+// 클래스형 멤버변수
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class DayOfYear
+{
+public:
+	DayOfYear(int monthValue, int dayValue);
+	DayOfYear(int monthValue);
+	DayOfYear();
+	void input();
+	void output();
+	int getMonthNumber();
+	int getDay();
+private:
+	int month, day;
+	void testDate();
+};
+
+
+int main() {
+	DayOfYear date1(2, 21), date2(5), date3;
+	cout << "초기화된 날짜들 : \n";
+	date1.output(); cout << endl;
+	date2.output(); cout << endl;
+	date3.output(); cout << endl;
+
+	date1 = DayOfYear(10, 31);
+	cout << "재설정된 날짜는 : \n";
+	date1.output(); cout << endl;
+	return 0;
+}
+
+DayOfYear::DayOfYear(int monthValue, int dayValue) : month(monthValue), day(dayValue)
+{
+	testDate();
+}
+
+DayOfYear::DayOfYear(int monthValue) : month(monthValue), day(1)
+{
+	testDate();
+}
+
+DayOfYear::DayOfYear() : month(1), day(1)
+{
+	testDate();
+}
+void DayOfYear::output()
+{
+	cout << month << "월 " << day << "일";
+}
+
+void DayOfYear::testDate()
+{
+	if (month < 1 || (month > 12))
+	{
+		cout << "illegal month value \n";
+		exit(1);
+	}
+	if ((day < 1) || (day > 31))
+	{
+		cout << "illegal day value\n";
+		exit(1);
+	}
+}
+
+
+
+
+
+
+
