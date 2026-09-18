bfa6ef43fe85ebd8913ee4226b05ab1e2bd378cc
solocarrot <whddbs3595@naver.com>
2026-09-18 12:55:15 +0900

    "과제시작함"

---
+++ Project4/3.findCharPosition.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	char array[100];
+	//엔터칠때까지입력받기?
+
+
+}
---
+++ Project4/printBinary.cpp
@@ -0,0 +1,34 @@
+	//#include <iostream>
+
+	//int main() {
+	//	int beCopiedArray[24];
+	//	int array[24];
+	//	int startNum = 24;
+
+	//	int n;
+	//	std::cin >> n;
+
+	//	int nForStart = n;
+	//	while (nForStart > 0) {
+	//		nForStart = nForStart / 2;
+	//		startNum--;
+	//	}
+
+	//	while (n > 0) {
+	//		int i;
+	//		beCopiedArray[i] = n % 2;
+	//		n = n / 2;
+	//		i++;
+	//	}
+
+	//	for(int i = 23; i >= startNum; i--) {
+	//		int j = 0;
+	//		array[i] = beCopiedArray[j];
+	//		j++;
+	//	}
+
+	//	for (int i = startNum; i < 24;i++) {
+	//		std::cout << array[i];
+	//	}
+	//
+	//}
