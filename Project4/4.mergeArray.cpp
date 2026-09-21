5dcd9cf2a659ac47135dfd5902d5f8da0eb30280
solocarrot <rlawhddbs3339@gmail.com>
2026-09-22 00:31:57 +0900

    "머지함수 짜는데 i j 로설정하고 하려니까 초과했을때 몰아서 넣는 코드고민중"

--- Project4/4.mergeArray.cpp
+++ Project4/4.mergeArray.cpp
@@ -1,25 +1,60 @@
-//#include<iostream>
-//using namespace std;
-//
-//bool readAscSortedArray(int[], int);
-//void mergeArray(int[], int[], int[], int);
-//void printArray(int[], int);
-//
-//int main() {
-//    const int SIZE = 5;
-//    int cnt, arr1[SIZE], arr2[SIZE], resultArr[SIZE * 2];
-//    cin >> cnt;
-//    for (int i = 0; i < cnt; ++i) 
-//    {
-//        if (readAscSortedArray(arr1, SIZE) && readAscSortedArray(arr2, SIZE)) 
-//        {
-//            mergeArray(arr1, arr2, resultArr, SIZE);
-//            printArray(resultArr, SIZE * 2);
-//        }
-//        else 
-//        { 
-//            cout << "[Error] Unsorted input\n ";
-//        }
-//    }
-//    return 0;
-//}
+#include<iostream>
+using namespace std;
+
+bool readAscSortedArray(int[], int);
+void mergeArray(int[], int[], int[], int);
+void printArray(int[], int);
+
+int main() {
+    const int SIZE = 5;
+    int cnt, arr1[SIZE], arr2[SIZE], resultArr[SIZE * 2];
+    cin >> cnt;
+    for (int i = 0; i < cnt; ++i) 
+    {
+        if (readAscSortedArray(arr1, SIZE) && readAscSortedArray(arr2, SIZE)) 
+        {
+            mergeArray(arr1, arr2, resultArr, SIZE);
+            printArray(resultArr, SIZE * 2);
+        }
+        else 
+        { 
+            cout << "[Error] Unsorted input\n ";
+        }
+    }
+    return 0;
+}
+
+bool readAscSortedArray(int arr[], int size) {
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                return false;
+                break;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
+void mergeArray(int arr1[], int arr2[], int resultArr[], int size)
+{
+    for (int i, j = 0; i + j < size * 2;) 
+    {
+        if (arr1[i] >= arr2[i]) 
+        {
+            resultArr[i + j] = arr1[i];
+            i++;
+        }
+        else 
+        {
+            resultArr[i + j] = arr2[j];
+            j++;
+        }
+    }
+
+}
--- Project4/단조증가확인용.cpp
+++ Project4/단조증가확인용.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
-using namespace std;
-
-bool sortedArray(int arr[], int MAX_LEN);
-
-int main() {
-	const int MAX_LEN = 5;
-	int arr[MAX_LEN];
-	for (int i = 0; i < 5; i++) {
-		cin >> arr[i];
-	}
-	
-	bool asnwer = sortedArray(arr, MAX_LEN);
-	cout << asnwer;
-
-
-}
-
-bool sortedArray(int arr[], int MAX_LEN) {
-	
-	int big = arr[4];
-	for (int i = 0; i < 4; i++) {
-		if (arr[i] > arr[i+1]) {
-			return false;
-			break;
-		}
-		else {
-			return true;
-		}
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//bool sortedArray(int arr[], int MAX_LEN);
+//
+//int main() {
+//	const int MAX_LEN = 5;
+//	int arr[MAX_LEN];
+//	for (int i = 0; i < 5; i++) {
+//		cin >> arr[i];
+//	}
+//	
+//	bool asnwer = sortedArray(arr, MAX_LEN);
+//	cout << asnwer;
+//
+//
+//}
+//
+//bool sortedArray(int arr[], int MAX_LEN) 
+//{
+//	for (int i = 0; i < 4; i++) 
+//	{
+//		if (arr[i] > arr[i+1]) 
+//		{
+//			return false;
+//			break;
+//		}
+//		else 
+//		{
+//			return true;
+//		}
+//	}
+//}
