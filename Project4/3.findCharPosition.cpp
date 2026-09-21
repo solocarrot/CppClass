7dfc01c8734b9e1460b423fffae54b19c22e967b
solocarrot <whddbs3595@naver.com>
2026-09-21 17:17:42 +0900

    "3번 풀려는데 아무런 생각이 안듦 지금뭐랄까 5살남자애한테 더하기 시키는 기분임 ㅇㅇ"

--- Project4/3.findCharPosition.cpp
+++ Project4/3.findCharPosition.cpp
@@ -1,8 +1,18 @@
-//#include <iostream>
-//
-//int main() {
-//	char array[100];
-//	//엔터칠때까지입력받기?
-//
-//
-//}
+#include <iostream>
+using namespace std;
+
+void printCharPosition(char str[], int MAX_LEN) {
+	const int ALPHABET_CNT = 26;
+	int alphabetFirstPosition[ALPHABET_CNT] = {};
+	for (int pos = 0; pos < MAX_LEN && str[pos] != '\0'; pos++) {
+		// 위치 저장하는 코드 구현
+	}
+	// 출력 코드 구현
+}
+int main() {
+	const int STR_MAX_LEN = 100;
+	char myCharArr[STR_MAX_LEN + 1]; // null저장을 위해 문자열의 최대 길이보다
+	// 하나 더 큰 배열을 사용해야 함
+	cin >> myCharArr;
+	printCharPosition(myCharArr, STR_MAX_LEN);
+	return 0;}
